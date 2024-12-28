ae3002ccf3b9535040af682e319d3181dbde5fa4
Asaf shapira <192441772+AsafShapi@users.noreply.github.com>
2024-12-28 23:30:31 +0200

    add Database and add OAuth to the database

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookiesession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURL)
+
 const app = express();
+
+app.use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+   
+      keys: [keys.cookieKey],
+    })
+  );
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes') (app);
 
